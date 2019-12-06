309a1e63174f117c8819930d5b92334fc81c5625
Carlos <stand1715@hotmail.com>
2019-12-06 17:27:09 +0800

    test

--- src/components/MaterialTabs.tsx
+++ src/components/MaterialTabs.tsx
@@ -145,12 +145,12 @@ const MaterialTabs: React.FC<Props> = ({
           keyboardShouldPersistTaps={keyboardShouldPersistTaps}
           scrollEnabled={scrollable}
         >
-          <Indicator
+          {/* <Indicator
             style={indicatorStyle}
             color={indicatorColor}
             value={indicatorPosition}
             tabWidth={!scrollable ? tabWidth : barWidth * 0.4}
-          />
+          /> */}
           <TabTrack barHeight={barHeight}>
             {items.map((item, idx) => (
               <Tab
